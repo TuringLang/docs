034b86374017c78ee1b50491ab3dfc6cdfec39a2
Cameron Pfiffer <cpfiffer@gmail.com>
2018-09-22 14:31:36 -0700

    Minor updates.

--- regression.jl
+++ regression.jl
@@ -31,7 +31,7 @@ delete!(data, :Student)
 head(data)
 
 # Split our dataset 70/30 into training/test sets.
-train, test = MLDataUtils.splitobs(data, at = 0.001);
+train, test = MLDataUtils.splitobs(data, at = 0.7);
 
 # Create our labels. These are the values we are trying to predict.
 train_label = train[:DefaultNum]
@@ -45,19 +45,42 @@ test = test[[:StudentNum, :Balance, :Income]]
 train = Matrix(train);
 test = Matrix(test);
 
+# # Bayesian logistic regression (LR)
+# @model lr_nuts(x, y, d, n, σ²) = begin
+#     α ~ Normal(0, σ²)
+#     β ~ MvNormal(zeros(d), σ² * ones(d))
+#
+#     for i = 1:n
+#         v = logistic(α + transpose(x[i,:]) * β)
+#         y[i] ~ Bernoulli(v)
+#     end
+# end
+
 # Bayesian logistic regression (LR)
 @model lr_nuts(x, y, d, n, σ²) = begin
     α ~ Normal(0, σ²)
-    β ~ MvNormal(zeros(d), σ² * ones(d))
+
+    βstudent ~ Normal(0, σ²)
+    βbalance ~ Normal(0, σ²)
+    βincome  ~ Normal(0, σ²)
+
     for i = 1:n
-        v = logistic(α + transpose(x[i,:]) * β)
+        v = logistic(α + βstudent*x[i, 1] + βbalance*x[i,2] + βincome*x[i,3])
         y[i] ~ Bernoulli(v)
     end
 end
 
 train = (train .- mean(train, dims=1)) ./ std(train, dims=1)
-
+Turing.setadbackend(:forward_diff)
 n, d = size(train)
-chain = sample(lr_nuts(train, train_label, d, n, 1), NUTS(500, 1.5))
-# chain = sample(lr_nuts(train, train_label, d, n, 1), HMC(1000, 0.05, 10))
+# chain = sample(lr_nuts(train, train_label, d, n, 1), NUTS(1000, 1.5))
+chain = sample(lr_nuts(train, train_label, d, n, 1), HMC(1000, 0.05, 10))
 describe(chain)
+
+# visualize the MCMC simulation results
+p1 = plot(chain);
+p2 = plot(chain, colordim = :parameter);
+
+# save to a png file
+savefig(p1, "demo-plot-parameters.png")
+savefig(p2, "demo-plot-chains.png")

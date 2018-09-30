2b65228e0a475c9e13de07ab0dc995f2ef53b41c
Cameron Pfiffer <cpfiffer@gmail.com>
2018-09-30 14:35:11 -0700

    Updates for new site style.

--- weave-examples.jl
+++ weave-examples.jl
@@ -8,11 +8,51 @@ markdown_path = joinpath(@__DIR__, "markdown")
 function polish_latex(path::String)
     txt = open(f -> read(f, String), path)
     open(path, "w+") do f
-        write(f, replace(txt, raw"$$" => raw"\$\$"))
+        txt = replace(txt, raw"$$" => raw"\$\$")
+
+        inline = r"(?<=[^\\])\$"
+        txt = replace(txt, inline => raw"$$")
+
+        write(f, txt)
     end
 end
 
-# Weave all examples
+function add_yaml(path)
+    lines = readlines(path, keep=true)
+
+    title = ""
+    permalink = "/:collection/:name/"
+
+    # Find the ID sections.
+    for i = 1:length(lines)
+        line = lines[i]
+
+        if startswith(line, "# ")
+            title = replace(line, "# " => "")
+            title = replace(title, "\n" => "")
+
+            lines[i] = ""
+            break
+        end
+    end
+
+    yaml_lines = ["---\n", "title: $title\n", "permalink: $permalink\n", "---\n"]
+
+    # Write lines back.
+    open(path, "w+") do f
+        # Prepend YAML.
+        for line in yaml_lines
+            write(f, line)
+        end
+
+        # Add remaining content.
+        for line in lines
+            write(f, line)
+        end
+    end
+end
+
+Weave all examples
 try
     for file in readdir(@__DIR__)
         if endswith(file, ".ipynb")
@@ -29,8 +69,9 @@ try
                     mod = Main)
 
                 polish_latex(out_path)
+                add_yaml(out_path)
             else
-                @warn "Not weaving $full_path as it has not been updated."
+                @warn "Skipping $full_path as it has not been updated."
             end
         end
     end

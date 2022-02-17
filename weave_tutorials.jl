e096abf1e39c364c8d438a7f417d132f4a5225c8
David Widmann <devmotion@users.noreply.github.com>
2022-02-17 23:29:30 +0100

    Update `weave_tutorials.jl`

--- weave_tutorials.jl
+++ weave_tutorials.jl
@@ -1,17 +1,16 @@
-
 using TuringTutorials
-
 target = ARGS[1]
-
 if isdir(target)
     if !isfile(joinpath(target, "Project.toml"))
-        error("cannot weave folder ", target, " without Project.toml!")
+        error("Cannot weave folder $(target) without Project.toml!")
     end
-    println("weaving the ", target, " folder")
+    println("Weaving the $(target) folder")
     TuringTutorials.weave_folder(target)
 elseif isfile(target)
-    println("weaving ", target)
-    TuringTutorials.weave_file(dirname(target), basename(target))
+    folder = dirname(target)[11:end] # remove the tutorials/
+    file = basename(target)
+    println("Weaving $(folder)/$(file)")
+    TuringTutorials.weave_file(folder, file)
 else
-    error("unable to find weaving target ", target)
+    error("Unable to find weaving target $(target)!")
 end

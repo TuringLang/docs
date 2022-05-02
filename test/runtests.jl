c038d6183028069e6da9f9e9d54e9a2d273551bc
David Widmann <david.widmann@it.uu.se>
2022-05-03 00:46:08 +0200

    Abort build process when error occurs

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,3 +36,37 @@ TuringTutorials.weave(
     out_path_root=pkgdir,
     build=(:script, :html, :github, :notebook),
 )
+
+write(
+    joinpath(tutorials_dir, "test_error.jmd"),
+    """
+    ---
+    title: Test
+    author: TuringLang team
+    weave_options:
+      error : false
+    ---
+
+    This is a test of the builder system.
+
+    The code throws an error.
+    ```julia
+    error("some error occurred")
+    ```
+
+    ```julia, echo=false, skip="notebook"
+    using TuringTutorials
+    TuringTutorials.tutorial_footer(WEAVE_ARGS[:folder], WEAVE_ARGS[:file])
+    ```
+    """,
+)
+
+# Generate default output
+for build in (:html, :github)
+    @test_throws ErrorException("some error occurred") TuringTutorials.weave(
+        tutorials_dir,
+        "test_error.jmd";
+        out_path_root=pkgdir,
+	build=(build,),
+    )
+end

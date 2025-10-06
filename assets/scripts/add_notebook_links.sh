d07ab29399c756d5755d0e3230180e8c919ee082
AoifeHughes <aoife1hughes@gmail.com>
2025-10-06 13:07:10 +0100

    made variable thingy in script.

--- assets/scripts/add_notebook_links.sh
+++ assets/scripts/add_notebook_links.sh
@@ -6,6 +6,9 @@ set -e
 
 echo "Adding notebook download links to HTML pages..."
 
+# Link text variable
+LINK_TEXT="Download notebook"
+
 # Find all HTML files that have corresponding .ipynb files
 find _site/tutorials _site/usage _site/developers -name "index.html" 2>/dev/null | while read html_file; do
     dir=$(dirname "$html_file")
@@ -14,11 +17,11 @@ find _site/tutorials _site/usage _site/developers -name "index.html" 2>/dev/null
     # Check if the corresponding .ipynb file exists
     if [ -f "$ipynb_file" ]; then
         # Check if link is already present
-        if ! grep -q 'Download notebook' "$html_file"; then
+        if ! grep -q "$LINK_TEXT" "$html_file"; then
             # Insert the notebook link AFTER the "Report an issue" link
             # This ensures it goes in the right place in the sidebar toc-actions
             # The download="index.ipynb" attribute forces browser to download instead of navigate
-            perl -i -pe 's/(<a href="[^"]*issues\/new"[^>]*><i class="bi[^"]*"><\/i>Report an issue<\/a><\/li>)/$1<li><a href="index.ipynb" class="toc-action" download="index.ipynb"><i class="bi bi-journal-code"><\/i>Download notebook<\/a><\/li>/g' "$html_file"
+            perl -i -pe "s/(<a href=\"[^\"]*issues\/new\"[^>]*><i class=\"bi[^\"]*\"><\/i>Report an issue<\/a><\/li>)/\$1<li><a href=\"index.ipynb\" class=\"toc-action\" download=\"index.ipynb\"><i class=\"bi bi-journal-code\"><\/i>$LINK_TEXT<\/a><\/li>/g" "$html_file"
             echo "  ✓ Added notebook link to $html_file"
         fi
     fi
